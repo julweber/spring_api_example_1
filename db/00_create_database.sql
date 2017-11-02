11bf1b93b8c4f15ab6b98ecb6e6c59f76957dcc3
Julian Weber <julianweberdev@gmail.com>
2017-11-02 16:36:57 +0100

    adds database scripts

---
+++ db/00_create_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE spring_api_example_1;
---
+++ db/01_create_customer.sql
@@ -0,0 +1,8 @@
+\c spring_api_example_1;
+
+CREATE TABLE customer(
+    id BIGINT PRIMARY KEY     NOT NULL,
+    email VARCHAR(30),
+    firstname VARCHAR(20),
+    lastname VARCHAR(20)
+);
