e8c4e5b305758b9d31d3a5274c6e1978b7b1e716
Julian Weber <julianweberdev@gmail.com>
2017-11-08 11:52:13 +0100

    adds QUEUE_NAME to env configuration within Configuration class

--- src/main/java/training/Configuration.java
+++ src/main/java/training/Configuration.java
@@ -6,11 +6,13 @@ public class Configuration {
 
   public final Integer RESULTS_PER_PAGE = (System.getenv("RESULTS_PER_PAGE") != null) ? Integer.parseInt(System.getenv("RESULTS_PER_PAGE")) : 5;
   public final String DEFAULT_GENRE = (System.getenv("DEFAULT_GENRE") != null) ? System.getenv("DEFAULT_GENRE") : "fusion";
+  public final String QUEUE_NAME = (System.getenv("QUEUE_NAME") != null) ? System.getenv("QUEUE_NAME") : "messages";
 
   public String toString() {
     String str = "";
     str += String.format("RESULTS_PER_PAGE: %d \n", RESULTS_PER_PAGE);
     str += String.format("DEFAULT_GENRE: %s \n", DEFAULT_GENRE);
+    str += String.format("QUEUE_NAME: %s \n", DEFAULT_GENRE);
     return str;
   }
 }
